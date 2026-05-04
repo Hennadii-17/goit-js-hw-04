dfc931fa6e7c16c9278260c161669da040264c3b
Hennadii <ecgena@gmail.com>
2026-05-04 23:21:08 +0100

    update hw

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,22 @@
-
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-   changeUsername(newName) {
-        this.username = newName;
-    },
-   updatePlayTime(hours) {
-        this.playTime += hours;
-    },
-    getInfo() {
-       return `${this.username} has ${this.playTime} active hours!`;
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+    for (const key in products) {
+        total += products[key];
     }
-};
+    return total <= containerSize;
+}
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
